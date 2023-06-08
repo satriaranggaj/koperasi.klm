a53e1622c8421703f8932d2d4d7438301d0f5141
satriaranggaj <satriarangga43@gmail.com>
2023-06-08 22:45:15 +0700

    asu

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -4,3 +4,14 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
 });
+
+window.onscroll = function(){
+    document.getElementById('navbar').style.boxShadow = '0px 3px 20px -10px black';
+
+    const navbarTop = document.getElementById('navbar');
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset === 0) {
+          navbarTop.style.boxShadow = 'none'; // Mengubah properti CSS saat scroll ke atas
+        }
+      });
+};
